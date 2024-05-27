1a51381a9195cf2e825287bd47bba62e59a4d678
nao9824 <k023g0055@g.neec.ac.jp>
2024-05-27 12:26:34 +0900

    横スクロール（カメラがプレイヤーに追従）を実装！！！！！！！！！！！

---
+++ Assets/Scripts/Ground/FollowCamera.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]  
+public class FollowCamera : MonoBehaviour
+{
+    GameObject playerObj;
+    PlayerController player;
+    Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        player = playerObj.GetComponent<PlayerController>();
+        playerTransform = playerObj.transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        //â°ÇæÇØí«è]
+        transform.position = new Vector3(playerTransform.position.x,transform.position.y,playerTransform.position.z-30);
+    }
+}
